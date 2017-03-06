74c3c5c5a5ee19114bf31d313ee12fc4a87f2e81
Neeraj-bhukania <bhukania.neeraj@gmail.com>
2017-03-07 01:20:34 +0530

    remapping number keys

---
+++ key.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# set up keyboard to exchange the Shift and Up keys, and the Down and Right keys
+xmodmap -e "keycode 87 = exclam KP_1"
+xmodmap -e "keycode 88 = at KP_2" 
+xmodmap -e "keycode 89 = numbersign KP_3"
+xmodmap -e "keycode 90 = Print KP_0" 
+xmodmap -e "keycode 83 = dollar KP_4" 
+xmodmap -e "keycode 84 = percent KP_5" 
+xmodmap -e "keycode 85 = asciicircum KP_6"
+xmodmap -e "keycode 79 = ampersand KP_7" 
+xmodmap -e "keycode 80 = parenleft KP_8" 
+xmodmap -e "keycode 81 = parenright KP_9"
+
